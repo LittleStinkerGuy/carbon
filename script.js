d4540917cb5f42e38a78aae7e06ce147a97eefc3
LittleStinkerGuy <127564718+LittleStinkerGuy@users.noreply.github.com>
2023-05-17 03:24:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function History(){
+    document.getElementById('pic')
+    .src="carbon4.png";}
+function Properties(){
+    document.getElementById('pic')
+    .src="carbon2.png";}
+function Where(){
+    document.getElementById('pic')
+    .src="carbon6.png";}
+function Usage(){
+    document.getElementById('pic')
+    .src="carbon5.png";}
